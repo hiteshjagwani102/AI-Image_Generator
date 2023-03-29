fa74cdd817d2602f3956499b4a74b0195624e740
Hitesh Jagwani <60232373+hiteshjagwani102@users.noreply.github.com>
2023-03-30 03:41:34 +0530

    second commit

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ const startServer = async () =>{
 
     try {
         connectDB(process.env.MONGODB_URL);
-        app.listen(8080, () => console.log("Server has started on port http://localhost:8080"))
+        app.listen(8080, () => console.log("Server started"))
     } catch (error) {
         console.log(err);
     }
